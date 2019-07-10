d0a6f0d19be249e727f3e7fb25e04010c7c78306
Daniel Mueller <deso@posteo.net>
2019-07-09 19:19:23 -0700

    Add first version of ratsio (i.e., NATS) based streaming
    This change introduces a first draft of a fully Rust based streaming
    infrastructure. We use the ratsio crate to interface with Polygon's NATS
    cluster.
    Please note that this change is intended solely for the sake of
    preservation of this work and documentation of how to work with ratsio.
    By now Polygon has enabled support for WebSockets and that is what we
    ultimately strive to use moving forward.

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,10 @@
 // Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::ffi::OsString;
-use std::io::Result as IoResult;
-
+use futures::Future;
 use futures::stream::Stream;
+
+use ratsio::error::RatsioError;
 
 use crate::env::api_info;
 use crate::error::Error;
@@ -18,23 +18,29 @@ use crate::events::Subscription;
 /// interacting with the Polygon API.
 #[derive(Debug)]
 pub struct Client {
-  api_key: OsString,
+  api_key: String,
 }
 
 impl Client {
   /// Create a new `Client` with information from the environment.
   pub fn from_env() -> Result<Self, Error> {
-    let api_key = api_info()?;
+    let api_key = api_info()?
+      .into_string()
+      .map_err(|_| Error::Str(format!("API key is not a valid utf-8 string").into()))?;
+
     Ok(Self { api_key })
   }
 
   /// Subscribe to the given stream in order to receive updates.
-  pub fn subscribe<'s, S>(
+  pub fn subscribe<S>(
     &self,
     subscriptions: S,
-  ) -> IoResult<impl Stream<Item = Event, Error = EventError>>
+  ) -> Result<
+    impl Future<Item = impl Stream<Item = Event, Error = EventError>, Error = RatsioError>,
+    Error,
+  >
   where
-    S: IntoIterator<Item = &'s Subscription>,
+    S: IntoIterator<Item = Subscription>,
   {
     subscribe(&self.api_key, subscriptions)
   }
--- src/events.rs
+++ src/events.rs
@@ -2,27 +2,42 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use std::error::Error as StdError;
-use std::ffi::OsStr;
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
-use std::io::Error as IoError;
-use std::io::Result as IoResult;
-use std::time::SystemTime;
 
-use futures::stream::once;
-use futures::stream::Stream;
+use futures::future::Either;
+use futures::future::err;
+use futures::future::ok;
+use futures::future::done;
+use futures::Future;
+use futures::stream::empty;
+use futures::Stream;
 
-use num_decimal::Num;
+use log::debug;
+
+use ratsio::error::RatsioError;
+use ratsio::nats_client::NatsClient;
+use ratsio::nats_client::NatsClientOptions;
+use ratsio::ops::Message;
+use ratsio::ops::Subscribe;
 
 use serde::Deserialize;
 use serde_json::Error as JsonError;
+use serde_json::from_slice as from_json;
 
+use crate::error::Error;
 use crate::stock::Aggregate;
 use crate::stock::Quote;
 use crate::stock::Trade;
 use crate::Str;
 
+
+const POLYGON_CLUSTER: [&str; 3] = [
+  "nats1.polygon.io:31101",
+  "nats2.polygon.io:31102",
+  "nats3.polygon.io:31103",
+];
 
 fn fmt_err(err: &dyn StdError, fmt: &mut Formatter<'_>) -> FmtResult {
   write!(fmt, "{}", err)?;
@@ -36,26 +51,22 @@ fn fmt_err(err: &dyn StdError, fmt: &mut Formatter<'_>) -> FmtResult {
 
 #[derive(Debug)]
 pub enum EventError {
-  Io(IoError),
   Json(JsonError),
+  Ratsio(RatsioError),
+  Str(Str),
 }
 
 impl Display for EventError {
   fn fmt(&self, fmt: &mut Formatter<'_>) -> FmtResult {
     match self {
-      EventError::Io(err) => fmt_err(err, fmt),
       EventError::Json(err) => fmt_err(err, fmt),
+      EventError::Ratsio(err) => write!(fmt, "{}", err),
+      EventError::Str(err) => write!(fmt, "{}", err),
     }
   }
 }
 
 impl StdError for EventError {}
-
-impl From<IoError> for EventError {
-  fn from(e: IoError) -> Self {
-    EventError::Io(e)
-  }
-}
 
 
 /// Possible subscriptions for a stock.
@@ -120,29 +131,168 @@ pub enum Event {
   Quote(Quote),
 }
 
+impl Event {
+  #[cfg(test)]
+  fn to_trade(&self) -> Option<&Trade> {
+    match self {
+      Event::Trade(trade) => Some(trade),
+      _ => None,
+    }
+  }
+}
+
+
+fn do_subscribe<S>(
+  client: &NatsClient,
+  subscriptions: S,
+) -> Result<impl Stream<Item = Message, Error = RatsioError>, RatsioError>
+where
+  S: IntoIterator<Item = Subscription>,
+{
+  let stream = empty();
+  let mut iter = subscriptions.into_iter();
+
+  // TODO: Right now we only support up to four subscriptions.
+  //       Unfortunately ratsio does not allow us to simply
+  //       subscribe to multiple sources exposed through one stream
+  //       and so we create a new stream for each subscription.
+  //       Because we want to expose all of them as a single stream
+  //       (of a single opaque type) we have to add each
+  //       subscription on demand.
+  let stream = if let Some(subscription) = iter.next() {
+    let other = client
+      .subscribe({
+        Subscribe::builder()
+          .subject(subscription.to_string().into())
+          .build()?
+      })
+      .flatten_stream();
+
+    Either::A(stream.select(other))
+  } else {
+    Either::B(stream)
+  };
+
+  let stream = if let Some(subscription) = iter.next() {
+    let other = client
+      .subscribe({
+        Subscribe::builder()
+          .subject(subscription.to_string().into())
+          .build()?
+      })
+      .flatten_stream();
+
+    Either::A(stream.select(other))
+  } else {
+    Either::B(stream)
+  };
+
+  let stream = if let Some(subscription) = iter.next() {
+    let other = client
+      .subscribe({
+        Subscribe::builder()
+          .subject(subscription.to_string().into())
+          .build()?
+      })
+      .flatten_stream();
+
+    Either::A(stream.select(other))
+  } else {
+    Either::B(stream)
+  };
+
+  let stream = if let Some(subscription) = iter.next() {
+    let other = client
+      .subscribe({
+        Subscribe::builder()
+          .subject(subscription.to_string().into())
+          .build()?
+      })
+      .flatten_stream();
+
+    Either::A(stream.select(other))
+  } else {
+    Either::B(stream)
+  };
+
+  assert_eq!(iter.next(), None, "only up to four subscriptions are supported currently");
+
+  Ok(stream)
+}
+
+
+fn subscribe_to_cluster<'c, C, S>(
+  api_key: &str,
+  cluster: C,
+  subscriptions: S,
+) -> Result<
+  impl Future<Item = impl Stream<Item = Event, Error = EventError>, Error = RatsioError>,
+  Error,
+>
+where
+  C: Into<Vec<String>>,
+  S: IntoIterator<Item = Subscription>,
+{
+  let options = NatsClientOptions::builder()
+    .echo(true)
+    .verbose(true)
+    .cluster_uris(cluster.into())
+    .auth_token(api_key)
+    .build()
+    .map_err(|err| Error::Str(err.into()))?;
+
+  debug!("NATS client options: {:?}", options);
+
+  let stream = NatsClient::from_options(options)
+    .and_then(|client| NatsClient::connect(&client))
+    // TODO: Use client.add_reconnect_handler?
+    .and_then(|client| {
+      let stream = do_subscribe(&client, subscriptions)
+        // TODO: Should not unwrap!
+        .unwrap()
+        .map_err(EventError::Ratsio)
+        .and_then(|msg| {
+          debug!("Received message: {:?}", &msg);
+          if msg.subject.starts_with("A.") {
+            done(from_json::<Aggregate>(&msg.payload)
+              .map(Event::SecondAggregate)
+              .map_err(EventError::Json))
+          } else if msg.subject.starts_with("AM.") {
+            done(from_json::<Aggregate>(&msg.payload)
+              .map(Event::MinuteAggregate)
+              .map_err(EventError::Json))
+          } else if msg.subject.starts_with("T.") {
+            done(from_json::<Trade>(&msg.payload)
+              .map(Event::Trade)
+              .map_err(EventError::Json))
+          } else if msg.subject.starts_with("Q.") {
+            done(from_json::<Quote>(&msg.payload)
+              .map(Event::Quote)
+              .map_err(EventError::Json))
+          } else {
+            err(EventError::Str(format!("received unexpected subject: {}", msg.subject).into()))
+          }
+        });
+
+      ok(stream)
+    });
+
+  Ok(stream)
+}
 
 /// Subscribe to and stream events from the Polygon service.
-pub fn subscribe<'s, S>(
-  api_key: &OsStr,
+pub fn subscribe<S>(
+  api_key: &str,
   subscriptions: S,
-) -> IoResult<impl Stream<Item = Event, Error = EventError>>
+) -> Result<
+  impl Future<Item = impl Stream<Item = Event, Error = EventError>, Error = RatsioError>,
+  Error,
+>
 where
-  S: IntoIterator<Item = &'s Subscription>,
+  S: IntoIterator<Item = Subscription>,
 {
-  // TODO: Right now we just return a stream with a single dummy trade.
-  //       We have to return something (as opposed to just not
-  //       implementing it), otherwise Rust cannot deduce the return
-  //       type properly.
-  let trade = Trade {
-    symbol: "MSFT".to_string(),
-    exchange: 4,
-    price: Num::from_int(100),
-    quantity: 1,
-    conditions: Vec::new(),
-    timestamp: SystemTime::now(),
-  };
-  let event = Event::Trade(trade);
-  Ok(once(Ok(event)))
+  let cluster = POLYGON_CLUSTER.iter().map(|x| x.to_string()).collect::<Vec<_>>();
+  subscribe_to_cluster(api_key, cluster, subscriptions)
 }
 
 
@@ -150,11 +300,120 @@ where
 mod tests {
   use super::*;
 
-  use std::os::unix::ffi::OsStrExt;
+  use std::collections::BTreeMap;
+  use std::net::SocketAddr;
+
+  use futures::future::ok;
+  use futures::Future;
+  use futures::Sink;
+  use futures::Stream;
+  use futures::sync::mpsc;
+
+  use num_decimal::Num;
+
+  use ratsio::codec::OpCodec;
+  use ratsio::error::RatsioError;
+  use ratsio::ops::Message;
+  use ratsio::ops::Op;
+  use ratsio::ops::ServerInfo;
 
   use serde_json::from_str as from_json;
 
-  use tokio::runtime::current_thread::block_on_all;
+  use test_env_log::test;
+
+  use tokio::runtime::Runtime;
+  use tokio::runtime::TaskExecutor;
+  use tokio_codec::Decoder;
+  use tokio_tcp::TcpListener;
+
+
+  fn serve_nats(
+    executor: TaskExecutor,
+    events: BTreeMap<&'static str, Vec<&'static str>>,
+  ) -> Result<SocketAddr, RatsioError>
+  {
+    let address = SocketAddr::from(([127, 0, 0, 1], 0));
+    let listener = TcpListener::bind(&address).unwrap();
+    let address = listener.local_addr().unwrap();
+    let executor2 = executor.clone();
+
+    let future = listener
+      .incoming()
+      .map(move |socket| OpCodec::default().framed(socket))
+      .from_err()
+      .and_then(|socket| {
+        socket.send(Op::INFO(ServerInfo {
+          server_id: "events-test".to_string(),
+          version: "0.1".to_string(),
+          go: "".to_string(),
+          host: "localhost".to_string(),
+          port: 0,
+          max_payload: usize::max_value(),
+          proto: 0,
+          client_id: 0,
+          auth_required: true,
+          tls_required: false,
+          tls_verify: false,
+          connect_urls: Vec::new(),
+        }))
+      })
+      .and_then(|socket| socket.send(Op::PING))
+      .and_then(move |socket| {
+        let (sink, stream) = socket.split();
+        let (send, recv) = mpsc::unbounded();
+        let recv = recv.map_err(|_| RatsioError::InnerBrokenChain);
+        let executor = executor.clone();
+        executor.spawn({
+          sink
+            .send_all(recv)
+            .map(|_| ())
+            .map_err(|err| assert!(false, "{:?}", err))
+        });
+        let mut events = events.clone();
+
+        stream.for_each(move |op| {
+          debug!("Got OP from client {:#?}", op);
+          match op {
+            Op::PONG => {
+              debug!("Got PONG from client");
+            }
+            Op::PING => {
+              let _ = send.unbounded_send(Op::PONG);
+            }
+            Op::SUB(cmd) => {
+              // Once we got a subscription request for a subject we
+              // remove that subject from the map of available ones.
+              match events.remove(cmd.subject.as_str()) {
+                Some(events) => {
+                  executor.spawn({
+                    for event in &events {
+                      let message = Message {
+                        subject: cmd.subject.clone(),
+                        sid: cmd.sid.clone(),
+                        reply_to: None,
+                        payload: event.as_bytes().to_vec(),
+                      };
+                      let _ = send.unbounded_send(Op::MSG(message));
+                    };
+                    ok(())
+                  })
+                },
+                None => panic!("encountered subscription for non-existent subject"),
+              }
+            }
+            _ => {}
+          }
+
+          ok(())
+        })
+      })
+      .into_future()
+      .map(|_| ())
+      .map_err(|_| ());
+
+    executor2.spawn(future);
+    Ok(address)
+  }
 
 
   #[test]
@@ -182,5 +441,45 @@ mod tests {
       },
       _ => panic!("unexpected event: {:?}", event),
     }
+  }
+
+  #[test]
+  fn receive_trades() {
+    let mut runtime = Runtime::new().unwrap();
+    let executor = runtime.executor();
+
+    let subscriptions = vec![
+      Subscription::Trades(Stock::Symbol("AAPL".into())),
+    ];
+    let mut events = BTreeMap::new();
+    events.insert(
+      "T.AAPL",
+      vec![
+        r#"{"ev":"T","sym":"AAPL","x":4,"p":194.949,"s":5,"c":[37],"t":1560181868990}"#,
+        r#"{"ev":"T","sym":"AAPL","x":12,"p":194.97,"s":22,"c":[37],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":12,"p":194.94,"s":6,"c":[37],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":12,"p":194.96,"s":60,"c":[37],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":12,"p":194.93,"s":29,"c":[37],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":12,"p":194.91,"s":71,"c":[14,37,41],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":11,"p":194.95,"s":100,"c":[14,41],"t":1560181869197}"#,
+        r#"{"ev":"T","sym":"AAPL","x":4,"p":194.955,"s":100,"c":[],"t":1560181869209}"#,
+      ],
+    );
+
+    let address = serve_nats(executor, events).unwrap().to_string();
+    let cluster = vec![address];
+    let future = subscribe_to_cluster("xxxx", cluster, subscriptions.into_iter()).unwrap()
+      .map_err(EventError::Ratsio)
+      .flatten_stream()
+      .skip(1)
+      .take(5)
+      .collect();
+
+    let events = runtime.block_on(future).unwrap();
+    assert_eq!(events.len(), 5);
+    let trade = events[0].to_trade().unwrap();
+    assert_eq!(&trade.symbol, "AAPL");
+    assert_eq!(trade.price, Num::new(19497, 100));
+    assert_eq!(trade.quantity, 22);
   }
 }
